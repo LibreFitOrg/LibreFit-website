9cf33215acc26ccb26e531dc776b1e17e155eff1
IamDg <iamdg.dev@proton.me>
2025-11-11 00:02:50 +0100

    fix: missing pages in vite config

--- vite.config.js
+++ vite.config.js
@@ -10,16 +10,19 @@ export default defineConfig({
         // This is the entry point
         main: resolve(__dirname, 'index.html'),
 
-
         privacy: resolve(__dirname, 'privacy.html'),
 
-
         contact: resolve(__dirname, 'contact.html'),
+
+        contactSuccess: resolve(__dirname, 'results/contact-success.html'),
+
+        contactFail: resolve(__dirname, 'results/contact-fail.html'),
+
+        contactInvalid: resolve(__dirname, 'results/contact-invalid-data.html'),
 
         support: resolve(__dirname, 'support.html'),
 
         license: resolve(__dirname, 'license-info.html'), 
-
 
         notfound: resolve(__dirname, '404.html'), 
       },
